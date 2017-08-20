1893f1e037e5f21ed5c4aec4674e0e522c23adc1
Airing <gx-deng@163.com>
2017-08-20 17:44:53 +0800

    实现约课发布与删除接口

--- routes/dating.js
+++ routes/dating.js
@@ -69,18 +69,42 @@ router.get('/create', function (req, res, next) {
             course_id: req.query.course_id
         }
     }).then(function(course) {
+        var dating = {
+            course: course,
+            courseId: course.id,
+            course_name: course.course_name,
+            course_content: course.course_content,
+            course_teacher: course.course_teacher,
+            dating_id: req.query.dating_id,
+            dating_rating: req.query.dating_rating,
+            dating_users: req.query.dating_users,
+            dating_time: req.query.dating_time,
+            dating_capacity: req.query.dating_capacity,
+            dating_register: req.query.dating_register,
+        }
 
+        DatingModel.create(dating).then(function() {
+            return res.jsonp({code: 0, msg: MESSAGE.SUCCESS})
+        });
     })
-    var dating = {
-        dating_id: req.query.dating_id,
-        dating_rating: req.query.dating_rating,
-        dating_users: req.query.dating_users,
-        dating_time: req.query.dating_time,
-        dating_capacity: req.query.dating_capacity,
-        dating_register: req.query.dating_register,
-    }
+});
 
-    DatingModel.create(dating).then(function() {
+router.get('/remove/:id', function (req, res, next) {
+
+    if (req.query.uid === undefined || req.query.uid === ''
+        || req.query.token === undefined || req.query.token === ''
+        || req.query.timestamp === undefined || req.query.timestamp === '') {
+        return res.jsonp({code: 1000, msg: MESSAGE.PARAMETER_ERROR});
+    }
+    
+    if(!checkToken(req.query.uid, req.query.timestamp, req.query.token)){
+        return res.jsonp({code: 403, msg: MESSAGE.TOKEN_ERROR})
+    }
+    DatingModel.destory({
+        where: {
+            id: req.params.id
+        }
+    }).then(function() {
         return res.jsonp({code: 0, msg: MESSAGE.SUCCESS})
     });
 });
